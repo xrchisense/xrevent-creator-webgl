f3d94fbbaa5e6648f9b744af8162defd3ee5f01d
Tom M <tom.milter@haw-hamburg.de>
2021-12-04 20:48:00 +0100

    Updated Creator-CustomUI to get Access to Room loader functions

--- Assets/xrchitecture/creator/shared/ui/webGLConnector/WebGLConnectorUI.cs
+++ Assets/xrchitecture/creator/shared/ui/webGLConnector/WebGLConnectorUI.cs
@@ -1,13 +1,18 @@
 #if UNITY_EDITOR
+using System;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(WebGLConnection))]
-public class WebGLConnectorUI:Editor
+public class WebGLConnectorUI : Editor
 {
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
+        GUILayout.Space(20);
+        GUILayout.Label("Spawn Primitive List:");
+        
+
         WebGLConnection myTarget = (WebGLConnection) target;
 
         foreach (var x in myTarget.PrefabList)
@@ -17,6 +22,38 @@ public class WebGLConnectorUI:Editor
                 myTarget.SpawnPrefab(x.name);
             }
         }
+        GUILayout.Space(20);
+        GUILayout.Label("GLTF Importer");
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Path to GLTF:     ",GUILayout.ExpandWidth(false));
+        string pathTOGLTF = GUILayout.TextField("test");
+        GUILayout.EndHorizontal();
+        if (GUILayout.Button("GLTFImport"))
+        {
+            myTarget.SpawnGltf(pathTOGLTF);
+        }
+        
+        GUILayout.Space(20);
+        GUILayout.Label("Persistent Manager Communication");
+        
+        
+        persistenceManager pm = myTarget.GetComponent<persistenceManager>();
+        string GUIDTextField = GUILayout.TextField(pm.getGUID());
+        
+        GUILayout.BeginHorizontal();
+        if(GUILayout.Button("Create new Room"))
+        {
+            myTarget.newRoom();
+        }
+        if(GUILayout.Button("Load a Room"))
+        {
+            myTarget.loadRoom(GUILayout.TextField(GUIDTextField));
+        }
+        GUILayout.EndHorizontal();
+
+
+
     }
 }
 #endif
