d2fd858853729a4f32225f30e866e25bdc147483
Tom M <tom.milter@haw-hamburg.de>
2022-02-12 19:12:45 +0100

    Videoscreen Custombehaivour (volume does not work)

--- Assets/xrchitecture/creator/common/sceneBuilder/userPrefabs/VideoWallItemBehaviour.cs
+++ Assets/xrchitecture/creator/common/sceneBuilder/userPrefabs/VideoWallItemBehaviour.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
+
+
 
 namespace Xrchitecture.Creator.Common.Data
 {
@@ -31,9 +30,18 @@ namespace Xrchitecture.Creator.Common.Data
 #if UNITY_EDITOR == false || UNITY_WEBGL == false
             //videowall add the Video Component:
             GameObject videoscreen = GetComponentInChildren<MeshRenderer>().gameObject;
+            var vid = videoscreen.gameObject.AddComponent<RenderHeads.Media.AVProVideo.MediaPlayer>();
+            var texture = videoscreen.gameObject.AddComponent<RenderHeads.Media.AVProVideo.ApplyToMaterial>();
+
+            texture.Material = videoscreen.gameObject.GetComponent<MeshRenderer>().material;
+            texture.Player = vid;
+            texture.TexturePropertyName = "_BaseMap";
+            vid.MediaSource = RenderHeads.Media.AVProVideo.MediaSource.Path;
+            vid.MediaPath.PathType = RenderHeads.Media.AVProVideo.MediaPathType.AbsolutePathOrURL;
+            vid.MediaPath.Path = url;
+
+            vid.AudioVolume = volume;
             
-                
-            vid.url = "https://www.youtube.com/watch?v=JsyyZ9DR_Us";
             vid.Play();
 #endif
         }
